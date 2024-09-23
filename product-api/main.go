22a6de4bd166eee1477c035985e9ec82556174a7
hnsia <44498178+hnsia@users.noreply.github.com>
2024-09-23 23:01:25 +0800

    feat: add simple get products endpoint

---
+++ product-api/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/hnsia/go-nic/product-api/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	ph := handlers.NewProducts(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+
+	s := http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		ErrorLog:     l,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		l.Println("Starting server on port :9090")
+
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan // blocking until a message is received
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+}
