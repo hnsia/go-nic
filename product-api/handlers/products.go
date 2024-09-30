b9a2090a281685bf2ffdc16a66d9704d8d62788f
hnsia <44498178+hnsia@users.noreply.github.com>
2024-09-30 23:20:42 +0800

    feat: complete add product

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -53,5 +53,5 @@ func (p *Products) addProduct(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Unable to unmarshal json", http.StatusBadRequest)
 	}
 
-	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
 }
