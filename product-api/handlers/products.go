0e6d0937df5a287e31e98b12f16f6191a981d765
hnsia <44498178+hnsia@users.noreply.github.com>
2024-10-09 23:16:05 +0800

    docs: add more annotation for swagger

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -26,6 +26,14 @@ import (
 	"github.com/hnsia/go-nic/product-api/data"
 )
 
+// A list of products returns in the response
+// swagger:response productsResponse
+type productsResponse struct {
+	// All products in the system
+	// in: body
+	Body []data.Product
+}
+
 // Products is a http.Handler
 type Products struct {
 	l *log.Logger
@@ -36,6 +44,12 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// swagger:route GET /products products listProducts
+// Returns a list of products
+// responses:
+//	200: productsResponse
+
+// GetProducts returns the products from the data store
 func (p *Products) GetProducts(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle GET Products")
 
