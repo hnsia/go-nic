68193ecb83c5a7184308397c16c298fc04a0e86b
hnsia <44498178+hnsia@users.noreply.github.com>
2024-10-16 22:07:26 +0800

    chore: setup new product image subproject

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -22,9 +23,13 @@ require (
 	github.com/go-openapi/spec v0.21.0 // indirect
 	github.com/go-openapi/validate v0.24.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/go-hclog v1.6.3 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/nicholasjackson/env v0.6.1 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	go.mongodb.org/mongo-driver v1.14.0 // indirect
---
+++ product-images/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/hashicorp/go-hclog"
+	"github.com/nicholasjackson/env"
+)
+
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+var logLevel = env.String("LOG_LEVEL", false, "debug", "Log output level for the server [debug, info, trace]")
+var basePath = env.String("BASE_PATH", false, "./imagestore", "Base path to save images")
+
+func main() {
+	env.Parse()
+
+	l := hclog.New(
+		&hclog.LoggerOptions{
+			Name:  "product-images",
+			Level: hclog.LevelFromString(*logLevel),
+		},
+	)
+
+	// create a logger for the server from the default logger
+	sl := l.StandardLogger(&hclog.StandardLoggerOptions{InferLevels: true})
+
+	// create the storage class, use local storage
+	// max filesize 5MB
+	stor, err := files.NewLocal(*basePath, 1024*1000*5)
+	if err != nil {
+		l.Error("Unable to create storage", "error", err)
+		os.Exit(1)
+	}
+
+	// create the handlers
+	fh := handlers.NewFiles(stor, l)
+
+	// create a new serve mux and register the handlers
+	sm := mux.NewRouter()
+
+	// filename regex: {filename:[a-zA-Z]+\\.[a-z]{3}}
+	// problem with FileServer is that it is dumb
+	ph := sm.Methods(http.MethodPost).Subrouter()
+	ph.HandleFunc("/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}", fh.ServeHTTP)
+
+	// get files
+	gh := sm.Methods(http.MethodGet).Subrouter()
+	gh.Handle(
+		"/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}",
+		http.StripPrefix("/images", http.FileServer(http.Dir(*basePath))),
+	)
+
+	// create a new server
+	s := http.Server{
+		Addr:         *bindAddress,
+		Handler:      sm,
+		ErrorLog:     sl,
+		IdleTimeout:  5 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// start the server
+	go func() {
+		l.Info("Starting server", "bind_address", *bindAddress)
+
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Error("Unable to start server", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	// trap sigterm or interrupt and gracefully shutdown the server
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan // blocking until a message is received
+	l.Info("Shutting down server with", "signal", sig)
+
+	// graceful shutdown the server, waiting max 30s for current operations to complete
+	tctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tctx)
+}
