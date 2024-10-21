fdd754a10cc972358ef879ae08cedb005c2526aa
hnsia <44498178+hnsia@users.noreply.github.com>
2024-10-22 00:12:52 +0800

    feat: setup gzipping

---
+++ product-images/handlers/zip_middleware.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+)
+
+type GzipHandler struct {
+}
+
+func (g *GzipHandler) GzipMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			w.Write([]byte("hello"))
+		}
+	})
+}
