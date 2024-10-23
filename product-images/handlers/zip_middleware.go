cce05f17aa788f5730303771bd45df0835381ea3
hnsia <44498178+hnsia@users.noreply.github.com>
2024-10-23 20:48:20 +0800

    feat: complete implementation of gzip middleware

--- product-images/handlers/zip_middleware.go
+++ product-images/handlers/zip_middleware.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"compress/gzip"
 	"net/http"
 	"strings"
 )
@@ -11,7 +12,41 @@ type GzipHandler struct {
 func (g *GzipHandler) GzipMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			w.Write([]byte("hello"))
+			// create a gzipped response
+			wrw := NewWrappedResponseWriter(w)
+
+			next.ServeHTTP(wrw, r)
+			defer wrw.Flush()
+			return
 		}
+
+		next.ServeHTTP(w, r)
 	})
+}
+
+type WrappedResponseWriter struct {
+	rw http.ResponseWriter
+	gw *gzip.Writer
+}
+
+func NewWrappedResponseWriter(w http.ResponseWriter) *WrappedResponseWriter {
+	gw := gzip.NewWriter(w)
+	return &WrappedResponseWriter{rw: w, gw: gw}
+}
+
+func (wr *WrappedResponseWriter) Header() http.Header {
+	return wr.rw.Header()
+}
+
+func (wr *WrappedResponseWriter) Write(d []byte) (int, error) {
+	return wr.gw.Write(d)
+}
+
+func (wr *WrappedResponseWriter) WriteHeader(statusCode int) {
+	wr.rw.WriteHeader(statusCode)
+}
+
+func (wr *WrappedResponseWriter) Flush() {
+	wr.gw.Flush()
+	wr.gw.Close()
 }
