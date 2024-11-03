0d2a2a4c6e65833b85cadfe76778015a4ec3cc4e
hnsia <44498178+hnsia@users.noreply.github.com>
2024-11-03 20:29:46 +0800

    feat: replace mock implementation of client get rates with actual get rates api call

--- currency/main.go
+++ currency/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/hashicorp/go-hclog"
+	"github.com/hnsia/go-nic/currency/data"
 	protos "github.com/hnsia/go-nic/currency/protos/currency/currency"
 	"github.com/hnsia/go-nic/currency/server"
 	"google.golang.org/grpc"
@@ -14,18 +15,31 @@ import (
 func main() {
 	logger := hclog.Default()
 
-	gs := grpc.NewServer()
-	cs := server.NewCurrency(logger)
+	rates, err := data.NewRates(logger)
+	if err != nil {
+		logger.Error("Unable to generate rates", "error", err)
+		os.Exit(1)
+	}
 
+	// create a new gRPC server, use WithInsecure to allow http connections
+	gs := grpc.NewServer()
+
+	// create an instance of the currency server
+	cs := server.NewCurrency(rates, logger)
+
+	// register the currency server
 	protos.RegisterCurrencyServer(gs, cs)
 
+	// register the reflection service which allows clients to determine the methods for this gRPC service
 	reflection.Register(gs) // Should disable this in production
 
+	// create a TCP socket for inbound server connections
 	l, err := net.Listen("tcp", ":9092")
 	if err != nil {
 		logger.Error("Unable to listen", "error", err)
 		os.Exit(1)
 	}
 
+	// listen for requests
 	gs.Serve(l)
 }
--- currency/server/currency.go
+++ currency/server/currency.go
@@ -4,20 +4,27 @@ import (
 	"context"
 
 	"github.com/hashicorp/go-hclog"
+	"github.com/hnsia/go-nic/currency/data"
 	protos "github.com/hnsia/go-nic/currency/protos/currency/currency"
 )
 
 type Currency struct {
-	log hclog.Logger
+	rates *data.ExchangeRates
+	log   hclog.Logger
 	protos.UnimplementedCurrencyServer
 }
 
-func NewCurrency(l hclog.Logger) *Currency {
-	return &Currency{l, protos.UnimplementedCurrencyServer{}}
+func NewCurrency(r *data.ExchangeRates, l hclog.Logger) *Currency {
+	return &Currency{r, l, protos.UnimplementedCurrencyServer{}}
 }
 
 func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos.RateResponse, error) {
 	c.log.Info("Handle GetRate", "base", rr.GetBase(), "destination", rr.GetDestination())
 
-	return &protos.RateResponse{Rate: 0.5}, nil
+	rate, err := c.rates.GetRate(rr.GetBase().String(), rr.GetDestination().String())
+	if err != nil {
+		return nil, err
+	}
+
+	return &protos.RateResponse{Rate: rate}, nil
 }
