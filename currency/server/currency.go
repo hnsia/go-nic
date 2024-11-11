334c12e1f2dec7ecb13b15643134c86c0606a09e
hnsia <44498178+hnsia@users.noreply.github.com>
2024-11-11 21:54:03 +0800

    feat: setup bidirectional streaming for currency rates

--- currency/server/currency.go
+++ currency/server/currency.go
@@ -2,10 +2,12 @@ package server
 
 import (
 	"context"
+	"time"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/hnsia/go-nic/currency/data"
 	protos "github.com/hnsia/go-nic/currency/protos/currency/currency"
+	"google.golang.org/grpc"
 )
 
 type Currency struct {
@@ -27,4 +29,15 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 	}
 
 	return &protos.RateResponse{Rate: rate}, nil
+}
+
+func (c *Currency) SubscribeRates(src grpc.BidiStreamingServer[protos.RateRequest, protos.RateResponse]) error {
+	for {
+		err := src.Send(&protos.RateResponse{Rate: 12.1})
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(5 * time.Second)
+	}
 }
